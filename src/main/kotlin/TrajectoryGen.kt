bc48ed6ea72704b975a47e377057eab3d1e250e2
Shishir Ghate <Shishir.Ghate@dowjones.com>
2020-01-04 16:58:58 -0600

    Added basic red path

--- src/main/kotlin/TrajectoryGen.kt
+++ src/main/kotlin/TrajectoryGen.kt
@@ -1,8 +1,5 @@
 import com.acmerobotics.roadrunner.geometry.Pose2d
 import com.acmerobotics.roadrunner.geometry.Vector2d
-import com.acmerobotics.roadrunner.path.heading.ConstantInterpolator
-import com.acmerobotics.roadrunner.path.heading.HeadingInterpolator
-import com.acmerobotics.roadrunner.path.heading.SplineInterpolator
 import com.acmerobotics.roadrunner.trajectory.Trajectory
 import com.acmerobotics.roadrunner.trajectory.TrajectoryBuilder
 import com.acmerobotics.roadrunner.trajectory.constraints.DriveConstraints
@@ -14,45 +11,53 @@ object TrajectoryGen {
     fun createTrajectory(): ArrayList<Trajectory> {
         val list = ArrayList<Trajectory>()
 
-        val builder1 = TrajectoryBuilder(Pose2d(-40.0, -63.0, 0.0.toRadians), constraints)
-        val builder2 = TrajectoryBuilder(Pose2d(-40.0, 63.0, 0.0.toRadians), constraints)
+        val builder1 = TrajectoryBuilder(Pose2d(-40.0, -63.0, Math.toRadians(0.0)), constraints)
+        val builder2 = TrajectoryBuilder(Pose2d(-40.0, -63.0, Math.toRadians(0.0)), constraints)
+        val builder3 = TrajectoryBuilder(Pose2d(-40.0, -63.0, Math.toRadians(0.0)), constraints)
 
         // dump routine
         builder1
-            .strafeTo(Vector2d(-22.0, -32.0)) // pick up first block
-            .lineTo(Vector2d(0.0, -40.0))
-            .splineTo(Pose2d(60.0, -30.0)) // drop off first block
+            .strafeTo(Vector2d(-22.0, -32.0))// pick up first block
+            .lineTo(Vector2d(0.0, -36.0))
+            .splineTo(Pose2d(55.0, -30.0)) // drop off first block
             .reverse() // drive backwards
-            .splineTo(Pose2d(0.0, -40.0))
+            .splineTo(Pose2d(0.0, -36.0))
             .lineTo(Vector2d(-46.0, -32.0)) // pick up second block
-            .reverse() // drive forwards
-            .splineTo(Pose2d(0.0, -40.0))
+            .reverse()
+            .splineTo(Pose2d(0.0, -36.0))
             .splineTo(Pose2d(50.0, -30.0)) // drop off second block
             .reverse() // drive backwards
-            .lineTo(Vector2d(42.0, -34.0), ConstantInterpolator(-90.0.toRadians)) // turn
-            .lineTo(Vector2d(42.0, -30.0), ConstantInterpolator(-90.0.toRadians)) // backup
-            .lineTo(Vector2d(42.0, -54.0), ConstantInterpolator(-180.0.toRadians)) // drag forward and turn
-            .lineTo(Vector2d(0.0, -40.0), ConstantInterpolator(-180.0.toRadians))
+            .strafeTo(Vector2d(42.0, -36.0)) // turn
 
         builder2
-            .strafeTo(Vector2d(-22.0, 32.0)) // pick up first block
-            .lineTo(Vector2d(0.0, 40.0))
-            .splineTo(Pose2d(60.0, 30.0)) // drop off first block
+            .strafeTo(Vector2d(-30.0, -32.0)) // pick up first block
+            .lineTo(Vector2d(0.0, -36.0))
+            .splineTo(Pose2d(55.0, -30.0)) // drop off first block
             .reverse() // drive backwards
-            .splineTo(Pose2d(0.0, 40.0))
-            .lineTo(Vector2d(-46.0, 32.0)) // pick up second block
-            .reverse() // drive forwards
-            .splineTo(Pose2d(0.0, 40.0))
-            .splineTo(Pose2d(50.0, 30.0)) // drop off second block
+            .splineTo(Pose2d(0.0, -36.0))
+            .lineTo(Vector2d(-54.0, -32.0)) // pick up second block
+            .reverse()
+            .splineTo(Pose2d(0.0, -36.0))
+            .splineTo(Pose2d(50.0, -30.0)) // drop off second block
             .reverse() // drive backwards
-            .lineTo(Vector2d(42.0, 34.0), ConstantInterpolator(90.0.toRadians)) // turn
-            .lineTo(Vector2d(42.0, 30.0), ConstantInterpolator(90.0.toRadians)) // backup
-            .lineTo(Vector2d(42.0, 54.0), ConstantInterpolator(180.0.toRadians)) // drag forward and turn
-            .lineTo(Vector2d(0.0, 40.0), ConstantInterpolator(180.0.toRadians))
+            .strafeTo(Vector2d(42.0, -36.0)) // turn
 
+        builder3
+            .strafeTo(Vector2d(-38.0, -32.0)) // pick up first block
+            .lineTo(Vector2d(0.0, -36.0))
+            .splineTo(Pose2d(55.0, -30.0)) // drop off first block
+            .reverse() // drive backwards
+            .splineTo(Pose2d(0.0, -36.0))
+            .lineTo(Vector2d(-62.0, -32.0)) // pick up second block
+            .reverse()
+            .splineTo(Pose2d(0.0, -36.0))
+            .splineTo(Pose2d(50.0, -30.0)) // drop off second block
+            .reverse() // drive backwards
+            .strafeTo(Vector2d(42.0, -36.0)) // turn
 
-        list.add(builder2.build())
         list.add(builder1.build())
+        list.add(builder2.build())
+        list.add(builder3.build())
         return list
     }
 
